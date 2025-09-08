68912b1a77876e4f0265eee7323e327c8c53fdf7
Andrés Contreras Guillén <me@ancongui.com>
2025-09-08 13:06:23 +0200

    Replace catalis references with firefly - core.catalis.vc -> core.getfirefly.io and catalis -> firefly

--- core-lending-mortgages-web/src/main/java/com/firefly/core/lending/mortgages/web/LendingMortgagesApplication.java
+++ core-lending-mortgages-web/src/main/java/com/firefly/core/lending/mortgages/web/LendingMortgagesApplication.java
@@ -53,7 +53,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
         ),
         servers = {
                 @Server(
-                        url = "http://core.catalis.vc/mortgages",
+                        url = "http://core.getfirefly.io/mortgages",
                         description = "Development Environment"
                 ),
                 @Server(
